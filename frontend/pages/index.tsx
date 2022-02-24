7e03e1d610acf8b326295a8f50ef7937e9cd7a15
Halunks <68344924+Halunks@users.noreply.github.com>
2022-02-24 17:15:29 +0100

    added noise-overlay

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,30 +5,15 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
     return (
         <div className={styles.container}>
-            <Head>
-                <title>About me</title>
-                <meta name="description" content="A web app about me built with Next.js"/>
-            </Head>
-            <main className={styles.main}>
-                <h2 className={styles.titleThree}>Lorem Ipsum Dolor</h2>
-                <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
-                    consectetur dolorem quibusdam rerum. Assumenda at, aut deserunt iure iusto natus!Lorem ipsum dolor
-                    sit amet, consectetur adipisicing elit. Alias
-                    consectetur dolorem quibusdam rerum. Assumenda at, aut deserunt iure iusto natus!</p>
-                <h2 className={styles.titleTwo}>Lorem Ipsum Dolor</h2>
-                <p className={styles.descriptionTwo}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
-                    consectetur dolorem quibusdam rerum. Assumenda at, aut deserunt iure iusto natus!Lorem ipsum dolor
-                    sit amet, consectetur adipisicing elit. Alias
-                    consectetur dolorem quibusdam rerum. Assumenda at, aut deserunt iure iusto natus!</p>
-                <h2 className={styles.titleThree}>Lorem Ipsum Dolor</h2>
-                <p className={styles.descriptionThree}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Alias
-                    consectetur dolorem quibusdam rerum. Assumenda at, aut <span className={styles.emphasis}>iusto
-                        natus!</span>Lorem ipsum dolor
-                    sit amet, consectetur adipisicing elit. Alias
-                    consectetur dolorem quibusdam rerum. Assumenda at, aut deserunt iure iusto natus!
-                </p>
-            </main>
+            <div className={styles.noise}>
+                <Head>
+                    <title>Lorem</title>
+                    <meta name="description" content="A web app about me built with Next.js"/>
+                </Head>
+                <main className={styles.main}>
+
+                </main>
+            </div>
         </div>
     )
 }
