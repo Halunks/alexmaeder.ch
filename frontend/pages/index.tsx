ac0b5a43bef2ede546d2286b23cfadd149eed867
Halunks <68344924+Halunks@users.noreply.github.com>
2022-02-25 16:47:37 +0100

    added banner

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,19 +1,20 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Banner from "../components/Banner";
 
 const Home: NextPage = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.noise}>
-                <Head>
-                    <title>Lorem</title>
-                    <meta name="description" content="A web app about me built with Next.js"/>
-                </Head>
-                <main className={styles.main}>
-
-                </main>
-            </div>
+            <Head>
+                <title>Lorem</title>
+                <meta name="description" content="A web app about me built with Next.js"/>
+            </Head>
+            <main className={styles.main}>
+                <div className={styles.noise}>
+                </div>
+                <Banner/>
+            </main>
         </div>
     )
 }
