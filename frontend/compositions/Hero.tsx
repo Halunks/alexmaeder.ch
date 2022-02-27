55b53094b5b78f00ba3c978c9cf369b3517bfc56
Halunks <68344924+Halunks@users.noreply.github.com>
2022-02-27 22:30:23 +0100

    changed color schema

--- frontend/compositions/Hero.tsx
+++ frontend/compositions/Hero.tsx
@@ -5,10 +5,12 @@ export default function Hero() {
     return (
         <section className={styles.section}>
             <p className={styles.preTitle}>Lorem Ipsum 2022</p>
-            <h1 className={styles.titleHero}>Lorem Ips Dolor Lorem Ipsum</h1>
-            <Link href="/link">
-                <div className={styles.buttonHero}>Ipsum Lorem</div>
-            </Link>
+            <h1 className={styles.titleHero}>Lorem Ips Dolor Lorem <span>Lor</span> Ipsum</h1>
+            <div className={styles.buttonHeroWrapper}>
+                <Link href="/link">
+                    <div className={styles.buttonHero}>Ipsum Lorem</div>
+                </Link>
+            </div>
         </section>
     )
 }
